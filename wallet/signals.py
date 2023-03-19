0335d2f3a35ec2623c6ab6ced58381a191d25215
tbro2020 <christian@tabaro.me>
2023-03-19 14:36:23 +0100

    Deploy

--- wallet/signals.py
+++ wallet/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from wallet.models import Transaction
@@ -6,5 +6,10 @@ from wallet import tasks
 
 
 @receiver(post_save, sender=Transaction)
-def post_product(sender, instance, created, **kwargs):
+def post_save_transaction(sender, instance, created, **kwargs):
+    tasks.solde.delay(instance.company_id)
+
+
+@receiver(post_delete, sender=Transaction)
+def post_delete_transaction(sender, instance, using):
     tasks.solde.delay(instance.company_id)
