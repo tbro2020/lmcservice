6f942e1d37de98cc09edfd4b0bbdf1124f334352
tbro2020 <christian@tabaro.me>
2023-03-19 16:03:20 +0100

    Deploy

--- core/views/api.py
+++ core/views/api.py
@@ -7,13 +7,14 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 
-Operation = apps.get_model("service", "operation")
-Product = apps.get_model("service", "product")
-CheckPoint = apps.get_model("service", "checkpoint")
-
 class OperationAPIView(APIView):
     def post(self, request):
         data = dict()
+
+        CheckPoint = apps.get_model("service", "checkpoint")
+        Operation = apps.get_model("service", "operation")
+        Product = apps.get_model("service", "product")
+
         data["operation"] = get_object_or_404(Operation, id=request.data["operation"])
         if "location" not in request.data or "device_info" not in request.data: raise Http404()
 
@@ -36,6 +37,9 @@ class OperationAPIView(APIView):
 
 class CheckPointATM(View):
     def get(self, request, checkpoint):
+        CheckPoint = apps.get_model("service", "checkpoint")
+        Product = apps.get_model("service", "product")
+
         checkpoint = get_object_or_404(CheckPoint, id=checkpoint)
         products = Product.objects.filter(operation=checkpoint.operation)
         if checkpoint.product: products = products.filter(id=checkpoint.product.id)
