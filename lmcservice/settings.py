bc0a9e9b2598f19385fd1eaa9d6295a9acf4b6ee
tbro2020 <christian@tabaro.me>
2023-03-19 23:47:19 +0100

    Deploy

--- lmcservice/settings.py
+++ lmcservice/settings.py
@@ -114,6 +114,8 @@ else:
     DATABASES = {
         'default': dj_database_url.config(default="DO_DATABASE_URL", conn_max_age=600, conn_health_checks=True, )}
 
+print(DATABASES)
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
