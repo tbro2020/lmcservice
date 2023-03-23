cfc1514c8a7d413ca7c8b72f15c236437b8bb35d
tbro2020 <christian@tabaro.me>
2023-03-23 04:07:11 +0100

    Deploy

--- lmcservice/celery.py
+++ lmcservice/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 from celery import Celery
-import os
+import os, ssl
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lmcservice.settings')
 
-app = Celery("lmcservice")
+app = Celery("lmcservice", broker_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE},
+             redis_backend_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE})
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
